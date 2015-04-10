51cefea0c734246836f7859159148f54387c1cfd
ChrisCo <ufcordle@hotmail.com>
2015-04-10 04:09:09 -0400

    refreshing the ol' portfoliolio

--- server/routes.js
+++ server/routes.js
@@ -10,9 +10,20 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
-  
+
+  app.route('/portfolio/r2pg/')
+    .get(function(req, res) {
+      res.sendfile('portfolio/r2pg/SmashBots/robot_frame.html');
+    });
+
+  app.route('/portfolio/r2pg/*')
+    .get(function(req, res) {
+      var subPath = req.path.split('/portfolio/r2pg/')[1];
+      res.sendfile('portfolio/r2pg/SmashBots/' + subPath);
+    });
+
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+  app.route('/:url(api|auth|portfolio|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
